d77603d3c7f914182e7987330ebe06a6792aa8dd
Patrick Leblanc <patrick@flagshipcompany.com>
2015-11-05 15:41:39 -0500

    provider in the spirit of Silex, using the BootableProviderInterface instead

--- src/TurbolinksServiceProvider.php
+++ src/TurbolinksServiceProvider.php
@@ -5,16 +5,23 @@ namespace Flagship\Components\Turbolinks;
 // Silex Centric Requires
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Silex\Api\BootableProviderInterface;
+use Silex\Application;
 // Lib Requires
 use Helthe\Component\Turbolinks\EventListener\TurbolinksListener;
 use Helthe\Component\Turbolinks\Turbolinks;
 
-class TurbolinksServiceProvider implements ServiceProviderInterface
+class TurbolinksServiceProvider implements ServiceProviderInterface, BootableProviderInterface
 {
     public function register(Container $app)
     {
-        $app->before(function () use ($app) {
-            $app['dispatcher']->addSubscriber(new TurbolinksListener(new Turbolinks()));
-        });
+        $app['turbolinks.listener'] = function () {
+            return new TurbolinksListener(new Turbolinks());
+        };
+    }
+
+    public function boot(Application $app)
+    {
+        $app['dispatcher']->addSubscriber($app['turbolinks.listener']);
     }
 }
